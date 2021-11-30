3cb78d5227a7fea4d73612f1fe6a8724c3b75454
AlejandroM12 <alejandro.morel1905@gmail.com>
2021-11-29 23:19:58 -0300

    Incorporando eventos && generando html con eventos

--- js/app.js
+++ js/app.js
@@ -1,48 +1,26 @@
 
-const cart; // CARRITO
-const btnCart; // CARRITO
-const btnPlus; // BOTONES DE AGREGAR O DISMINUIR PRODUCTO - CONTADOR DE PRODUCTOS
-const btnMinus; // BOTONES DE AGREGAR O DISMINUIR PRODUCTO - CONTADOR DE PRODUCTOS
-const productCounter; // BOTONES DE AGREGAR O DISMINUIR PRODUCTO - CONTADOR DE PRODUCTOS
-const gallery;// SECCION GALERIA DE IMAGEN
-const heroImg;// SECCION GALERIA DE IMAGEN
-const btnNext; // BOTONES DE SIGUIENTE O ANTERIOR PARA LA GALERIA DE IMAGENES
-const btnPrevious; // BOTONES DE SIGUIENTE O ANTERIOR PARA LA GALERIA DE IMAGENES
-const btnAddToCart; // BOTON AGREGAR AL CARRITO
-const cartCount; // CONTADOR DEL CARRITO
+// BOTONES DE AGREGAR O DISMINUIR PRODUCTO - CONTADOR DE PRODUCTOS
+const btnPlus = document.querySelector('#btnPlus'); 
+const btnMinus = document.querySelector('#btnMinus'); 
+const productCounter = document.querySelector('.counter'); 
 
-// VARIABLES NUMERICAS
 let productCounterValue = 1;
-let productsInCart = 0;
-let price =  250.00;
-let discount = 0.5;
 
-// FUNCION PARA ABRIR EL CARRITO
-function openCart(){
+btnPlus.addEventListener('click', productCounterPlus);
+btnMinus.addEventListener('click', productCounterMinus);
+
+function productCounterPlus(){
+    setProductCounter(1);
 
 }
-
-
-// FUNCION PARA AGREGAR AL CARRITO
-
-function addToCart(){
-    productsInCart += productCounterValue;
-
-    const productHTMLElement = `
-    <div class="item">
-          <img class="product-img" src="./images/image-product-1-thumbnail.jpg" alt="product 1">
-          <div class="details">
-            <div class="product-name">Autumn Limited Edition...</div>
-            <div class="price-group">
-              <div class="price">$${(price*discount).toFixed(2)}</div> x
-              <div class="count">${productsInCart}</div>
-              <div class="total-amount">$${(price*discount*productsInCart).toFixed(2)}</div>
-          </div>
-          </div>
-            <img id="btnDelete" src="./images/icon-delete.svg" alt="icon delete">
-        </div>
-        `;
-        
+function productCounterMinus(){
+    setProductCounter(-1);
+}
+function setProductCounter(value){
+    if((productCounterValue + value) > 0){
+        productCounterValue += value;
+        productCounter.innerHTML = productCounterValue;
+    }
 }
 
 
--- js/collections.js
+++ js/collections.js
@@ -1,71 +1,60 @@
-const products = [
-    {
-        name: "Zapatilla Urbana",
-        marca: "Nike",
-        precio: "$4000"
-    },
-    {
-        name: "Zapatilla Urbana",
-        marca: "Adidas",
-        precio: "$8000"
-    },
-    {
-        name: "Zapatilla Deportiva",
-        marca: "Nike",
-        precio: "$4000"
-    },
-    {
-        name: "Zapatilla Urbana",
-        marca: "Adidas",
-        precio: "$5000"
-    },
-    {
-        name: "Zapatilla Deportiva",
-        marca: "Adidas",
-        precio: "$5000"
-    },
-    {
-        name: "Zapatilla Urbana",
-        marca: "Nike",
-        precio: "$4000"
-    },
-    {
-        name: "Zapatilla Deportiva",
-        marca: "Nike",
-        precio: "$5000"
-    },
-    {
-        name: "Zapatilla Deportiva",
-        marca: "Nike",
-        precio: "$9000"
-    },
-    {
-        name: "Zapatilla Urbana",
-        marca: "Adidas",
-        precio: "$5000"
-    },
-    {
-        name: "Zapatilla Urbana",
-        marca: "Nike",
-        precio: "$8000"
-    },
-    {
-        name: "Zapatilla Urbana",
-        marca: "Nike",
-        precio: "$5000"
-    },
+const row = document.getElementById('row');
+const form = document.getElementById('product-form');
 
-];
 
-let tableBody = document.getElementById('table-body')
-let productMap = products.map( product =>{
-    console.log(products)
-    return `
-        <tr>
-            <td>${product.name}</td>
-            <td>${product.marca}</td>
-            <td>${product.precio}</td>
-        </tr>
-    `
-}).join('')
-tableBody.innerHTML = productMap;
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let title = document.getElementById('title-form').value;
+    let description = document.getElementById('description-form').value;
+
+    create_card(title,description);
+})
+
+// CREAR CARTA
+function create_card(title,description){
+    let html = `        
+                  
+                        <div class="col-sm-6 col-md-4 mt-5" id="cardFirst">
+                          <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                              <h5 class="title_card">${title}</h5>
+                              <p class="card-text">${description}</p>
+                              <a href="#" class="btn btn-primary" id="btnProd">Eliminar Producto</a>
+                            </div>
+                          </div>
+                          </div>
+                          
+                          
+                        
+    `;
+    row.innerHTML += html;
+} 
+
+// ELIMINAR CARTA
+
+
+btn.addEventListener('click', (e)=>{
+  
+  let ancestor = get_ancestors(e.target, 3);
+  row.removeChild(ancestor);
+});
+
+function get_ancestors(ancestor,indice){
+  if( indice == 0){
+   
+    return ancestor;
+    
+  }
+  indice --;
+  return get_ancestors(ancestor.parentElement, indice);
+}
+// EVENTO DEL MOUSE
+
+
+btn.addEventListener('mouseenter', function(){
+  this.className = 'btn btn-danger';
+});
+btn.addEventListener('mouseout', function(){
+  this.className = 'btn btn-primary';
+});
